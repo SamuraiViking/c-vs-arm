a321c054a98a52fe9db07fca1bf34d737f6073d7
SamuraiViking <nelson67@stolaf.edu>
2019-12-02 22:45:07 -0600

    change vs to to

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ function Header() {
                 <img className="logo" src={cLogo} alt="c logo"/>
             </div>
             <div className="versus-text">
-                <span>VS</span>
+                <span>TO</span>
             </div>
             <div className="asm-logo">
                 <img className="logo" src={asmLogo} alt="c logo"/>
--- src/snippets/C/cInputOutput.js
+++ src/snippets/C/cInputOutput.js
@@ -6,7 +6,7 @@ int main() {
     int x, y, z;
     printf("Enter 3 numbers: ");
     scanf("%d %d %d", &x, &y, &z);
-    printf("You entered the numbers: %d %d and %d\n", x, y, z);
+    printf("You entered the numbers: %d %d and %d\\n", x, y, z);
 
     return 0;
 }
